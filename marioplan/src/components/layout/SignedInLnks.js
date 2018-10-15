27c4d6e68637e62612f57a95b45316771c5b0539
tony2tones <anthony.marque@gmail.com>
2018-10-15 12:21:56 +0200

    updated li to be clickable links using NavLink correctly

--- marioplan/src/components/layout/SignedInLnks.js
+++ marioplan/src/components/layout/SignedInLnks.js
@@ -5,8 +5,8 @@ import './layout.css';
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/' />New Project</li>
-      <li><NavLink to='/' />Log Out</li>
+      <li><NavLink to='/'>New Project</NavLink></li>
+      <li><NavLink to='/'>Log Out</NavLink></li>
       <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
     </ul>
   );
--- marioplan/src/components/layout/SignedOutLinks.js
+++ marioplan/src/components/layout/SignedOutLinks.js
@@ -5,8 +5,8 @@ import './layout.css';
 const SignedOutLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/' />Sign up</li>
-      <li><NavLink to='/' />Login</li>
+      <li><NavLink to='/'>Sign up</NavLink></li>
+      <li><NavLink to='/'>Login</NavLink></li>
     </ul>
   );
 };
