61181123586b0410994d14e57be56e465d3e21e5
tony2tones <anthony.marque@gmail.com>
2018-10-15 13:44:26 +0200

    added Switch for better routing

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from './components/layout/Navbar';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import Notifications from "./components/dashboard/Notifications";
 
 class App extends Component {
   render() {
@@ -8,6 +11,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path='/' exact component={Dashboard} />
+            <Route path='/Notifications' exact component={Notifications} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <projectList />
+            <ProjectList />
           </div>
           <div className="col s12 m5 offset-m1" />
           <Notifications />
