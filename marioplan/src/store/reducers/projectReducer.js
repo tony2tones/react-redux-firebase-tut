4f7e62180850ce4d798c64cd111b0005b5cb1d5c
tony2tones <anthony.marque@gmail.com>
2018-10-17 10:22:41 +0200

    added providers and reducers to the index.js

---
+++ marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    projects: [
+        {id:'1', title}
+    ]
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
