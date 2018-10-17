046f34799a6194ac92986e1d1c135156fa0d58be
tony2tones <anthony.marque@gmail.com>
2018-10-17 15:40:25 +0200

    adding redux store

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,6 +1,8 @@
 const initState = {
     projects: [
-        {id:'1', title}
+        {id:'1', title: 'Toasty toast toast', content: 'yadda yadda yadda yadda'},
+        {id:'2', title: 'Toasty ', content: 'yadda yadda yadda yadda'},
+        {id:'3', title: 'Toby Toby', content: 'yadda yadda yadda yadda'}
     ]
 };
 
--- marioplan/src/store/reducers/rootReducer.js
+++ marioplan/src/store/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
-import { combineReducers } from "./redux";
+import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
     auth: authReducer,
