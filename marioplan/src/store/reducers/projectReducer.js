176573de04f4283b5c1e736e58ddeb1e05d8f480
tony2tones <anthony.marque@gmail.com>
2018-10-29 10:30:58 +0200

    Removed Dummy data

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,14 +1,4 @@
-const initState = {
-  projects: [
-    {
-      id: "1",
-      title: "Toasty toast toast",
-      content: "yadda yadda yadda yadda"
-    },
-    { id: "2", title: "Toasty ", content: "yadda yadda yadda yadda" },
-    { id: "3", title: "Toby Toby", content: "yadda yadda yadda yadda" }
-  ]
-};
+const initState = {}
 /*eslint-disable */
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
