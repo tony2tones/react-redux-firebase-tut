83cefa7607ad9c408d44c325866711991bb64f08
tony2tones <anthony.marque@gmail.com>
2018-11-02 08:56:07 +0200

    working on Sign out links and functionality

--- marioplan/src/components/layout/SignedInLnks.js
+++ marioplan/src/components/layout/SignedInLnks.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './layout.css';
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
+
+import "./layout.css";
 
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/create'>New Project</NavLink></li>
-      <li><NavLink to='/'>Log Out</NavLink></li>
-      <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
+      <li>
+        <NavLink to="/create">New Project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating pink lighten-1">
+          NN
+        </NavLink>
+      </li>
     </ul>
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(mapDispatchToProps)(SignedInLinks);
